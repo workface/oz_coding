dc5298406129096e70613acdcbfdace15629e3df
workface <with.you70@gmail.com>
2025-03-03 08:21:09 +0900

    "feat:session을 이용해 login페이지 만들기실습"
    -session을 이용해 login페이지 만들기 실습

---
+++ FLASK/Part5/auth_session/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, session, flash
+
+app = Flask(__name__)
+
+app.secret_key = 'flask-secret-key' # 실제로 배포시 .env 나 yaml
+
+users = {
+    'john':'pw123',
+    'leo':'pw345'
+}
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/login', methods = ['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    
+    if username in users and users[username] == password:
+        session['username'] = username
+
+        return redirect('/secret')
+    else:
+        flash("Invalid username or password")
+        return redirect('/')
+    
+@app.route('/secret')
+def secret():
+    if 'username' in session:
+        return render_template('secret.html')
+    else:
+        return redirect('/')
+    
+#로그아웃
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/')
+
+if __name__ =='__main__':
+    app.run(debug=True)
